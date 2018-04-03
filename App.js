acb2b970f08f30aefd87a34f7a18cb33c20e8f44
ashwiniks <ashwini.jss@gmail.com>
2018-04-04 04:39:29 +0530

    hotfix

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
        
-        <Home name="ashwini"/>
+        <Home name="ashwinigit hotfix"/>
       </View>
     );
   }
