1b447a0b55ed05da7b3ed9a34772aad1ac54b7fa
ashwiniks <ashwini.jss@gmail.com>
2018-04-05 00:43:56 +0530

    in

--- Home.js
+++ Home.js
@@ -23,7 +23,7 @@ class Home extends Component {
              <Image style = {styles.img} source = {require('./img/studio.png')} />
             <TextInput style = {styles.input}
                underlineColorAndroid = "transparent"
-               placeholder = "Email"
+               placeholder = "Email change in this branch"
                placeholderTextColor = "#9a73ef"
                autoCapitalize = "none"
                onChangeText = {this.handleEmail}/>
